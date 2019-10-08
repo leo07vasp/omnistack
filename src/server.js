a161156a82845f1a7fc0712a9e6583adb3206553
Leonardo <leo07vasp@gmail.com>
2019-10-08 17:36:31 -0300

    start

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect(
+  'mongodb+srv://omnistack:omnistack@omnistack-h0oyc.mongodb.net/semana09?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+// GET, POST, PUT, DELETE
+
+// req.query = acessar query params url req.query.id
+// req.params = acessar route params app.put('usr/:id') => req.params.id
+
+app.use(cors());
+// app.use(cors({ origin: 'http://localhost' }));
+app.use(express.json());
+app.use(routes);
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.listen(3333, () => {
+  console.log('listen on 3333');
+});
